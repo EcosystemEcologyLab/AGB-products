ca9302144eabb302ae71206cbe6efcd3b053ba58
Eric Scott <scottericr@gmail.com>
2024-06-19 15:12:34 -0700

    use file_fast (I think I can trust timestamps on snow)

--- _targets.R
+++ _targets.R
@@ -20,23 +20,23 @@ tar_source()
 root <- "/Volumes/moore/"
 tar_plan(
   #track input files
-  tar_file(liu_file, path(root, "Liu/Aboveground_Carbon_1993_2012.nc")),
-  tar_file(xu_file, path(root, "Xu/test10a_cd_ab_pred_corr_2000_2019_v2.tif")),
-  tar_file(chopping_file, path(root, "Chopping/MISR_agb_estimates_20002021.tif")),
-  tar_file(gedi_file, path(root, "GEDI_L4B_v2.1/data/GEDI04_B_MW019MW223_02_002_02_R01000M_MU.tif")),
-  tar_file(menlove_file, path(root, "Menlove/data/")),
+  tar_file_fast(liu_file, path(root, "Liu/Aboveground_Carbon_1993_2012.nc")),
+  tar_file_fast(xu_file, path(root, "Xu/test10a_cd_ab_pred_corr_2000_2019_v2.tif")),
+  tar_file_fast(chopping_file, path(root, "Chopping/MISR_agb_estimates_20002021.tif")),
+  tar_file_fast(gedi_file, path(root, "GEDI_L4B_v2.1/data/GEDI04_B_MW019MW223_02_002_02_R01000M_MU.tif")),
+  tar_file_fast(menlove_file, path(root, "Menlove/data/")),
   #these ones come as multiple files and need some special handling to iterate over each tile
   tar_target(esa_paths_files, get_esa_paths(), iteration = "list"),
-  tar_target(esa_paths, esa_paths_files, pattern = map(esa_paths_files), format = "file"),
-  tar_files(ltgnn_paths, fs::dir_ls(path(root, "LT_GNN"), glob = "*.zip")),
+  tar_target(esa_paths, esa_paths_files, pattern = map(esa_paths_files), format = "file_fast"),
+  tar_files(ltgnn_paths, fs::dir_ls(path(root, "LT_GNN"), glob = "*.zip"), format = "file_fast"),
   
   #track output files
-  tar_file(liu, clean_liu(input = liu_file)),
-  tar_file(xu, clean_xu(input = xu_file)),
-  tar_file(chopping, clean_chopping(input = chopping_file)),
-  tar_file(gedi, clean_gedi(input = gedi_file)),
-  tar_file(menlove, clean_menlove(input = menlove_file)),
+  tar_file_fast(liu, clean_liu(input = liu_file)),
+  tar_file_fast(xu, clean_xu(input = xu_file)),
+  tar_file_fast(chopping, clean_chopping(input = chopping_file)),
+  tar_file_fast(gedi, clean_gedi(input = gedi_file)),
+  tar_file_fast(menlove, clean_menlove(input = menlove_file)),
   #these iterate over tiles and save output as tiles
-  tar_file(esa, clean_esa(esa_paths), pattern = map(esa_paths)),
-  tar_file(ltgnn, clean_ltgnn(ltgnn_paths), pattern = map(ltgnn_paths))
+  tar_file_fast(esa, clean_esa(esa_paths), pattern = map(esa_paths)),
+  tar_file_fast(ltgnn, clean_ltgnn(ltgnn_paths), pattern = map(ltgnn_paths))
 )
