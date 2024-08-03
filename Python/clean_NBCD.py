fb34a9363249295097172e6e7a6f9bc0cdab48c2
Yang Li <yangligeo@arizona.edu>
2024-08-02 17:13:37 -0700

    nothing to do with this product at this repo

---
+++ Python/clean_NBCD.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from __init__ import *
+
+datadir = join(data_root, 'Natl Biomass Carbon Dataset')
+
+def Nothing_to_do():
+    print('Nothing to do')
+
+if __name__ == '__main__':
+    Nothing_to_do()
